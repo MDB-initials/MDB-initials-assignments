826ca47c5fe85a843f23132a067d920f3fd41919
MDB-initials <84526346+MDB-initials@users.noreply.github.com>
2021-05-26 17:59:30 -0400

    calculator as close to finished as im going to try
    know issues:
    large numbers ignore decimals for some reason
    no way to 'undo'
    cant swap to negatives at will
    clicking decimal button after equals button results in a zero appearing that disappears if you press a number then reappears if you press another

---
+++ calc1.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="notesCont.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <br />
+    <!-- <div style="width:880px"> -->
+    <p>
+        <asp:TextBox ID="txtDisplay" runat="server" Height="35px" Width="270px">0</asp:TextBox>
+    </p>
+    <p>
+        <asp:Button ID="btn7" runat="server" Height="35px" Text="7" Width="70px" OnClick="btn7_Click" />
+        <asp:Button ID="btn8" runat="server" Height="35px" Text="8" Width="70px" OnClick="btn8_Click" />
+        <asp:Button ID="btn9" runat="server" Height="35px" Text="9" Width="70px" OnClick="btn9_Click" />
+        <asp:Button ID="div" runat="server" Height="35px" Text="÷" Width="70px" OnClick="div_Click" />
+    </p>
+    <p>
+        <asp:Button ID="btn4" runat="server" Height="35px" Text="4" Width="70px" OnClick="btn4_Click" />
+        <asp:Button ID="btn5" runat="server" Height="35px" Text="5" Width="70px" OnClick="btn5_Click" />
+        <asp:Button ID="btn6" runat="server" Height="35px" Text="6" Width="70px" OnClick="btn6_Click" />
+        <asp:Button ID="multi" runat="server" Height="35px" Text="x" Width="70px" OnClick="multi_Click" />
+    </p>
+    <p>
+        <asp:Button ID="btn1" runat="server" Height="35px" Text="1" Width="70px" OnClick="btn1_Click" />
+        <asp:Button ID="btn2" runat="server" Height="35px" Text="2" Width="70px" OnClick="btn2_Click" />
+        <asp:Button ID="btn3" runat="server" Height="35px" Text="3" Width="70px" OnClick="btn3_Click" />
+        <asp:Button ID="min" runat="server" Height="35px" Text="-" Width="70px" OnClick="min_Click" />
+    </p>
+    <p>
+        <asp:Button ID="btn0" runat="server" Height="35px" Text="0" Width="70px" OnClick="btn0_Click" />
+        <asp:Button ID="dec" runat="server" Height="35px" Text="." Width="70px" OnClick="dec_Click" />
+        <asp:Button ID="plus" runat="server" Height="35px" Text="+" Width="70px" OnClick="plus_Click" />
+        <asp:Button ID="equal" runat="server" Height="35px" Text="=" Width="70px" BackColor="Orange" OnClick="equal_Click" />
+    </p>
+        <!-- </div> -->
+</asp:Content>
---
+++ calc2.aspx.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Web.UI;
+
+namespace notesCont
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text == "0") { 
+                Session["num"] = "y";
+            }
+        }
+
+        protected void div_Click(object sender, EventArgs e)
+        {
+            Session["op"] = "÷";
+            Session["decimal"] = null;
+        }
+
+        protected void multi_Click(object sender, EventArgs e)
+        {
+            Session["op"] = "x";
+            Session["decimal"] = null;
+        }
+
+        protected void min_Click(object sender, EventArgs e)
+        {
+            Session["op"] = "-";
+            Session["decimal"] = null;
+        }
+
+        protected void plus_Click(object sender, EventArgs e)
+        {
+            Session["op"] = "+";
+            Session["decimal"] = null;
+        }
+
+        protected void equal_Click(object sender, EventArgs e)
+        {
+            try {
+            float fv;
+            try {
+                fv = float.Parse(Session["FirstValue"].ToString());
+            } catch
+            {
+                fv = 0;
+            }
+            float sv = float.Parse(Session["SecondValue"].ToString());
+            string op = Session["op"].ToString();
+            switch (op)
+            {
+                case "÷":
+                    if (fv != 0 && sv != 0) {
+                    txtDisplay.Text = (fv / sv).ToString();
+                    } else
+                    {
+                        txtDisplay.Text = "0";
+                    }
+                    break;
+                case "x":
+                    txtDisplay.Text = (fv * sv).ToString();
+                    break;
+                case "-":
+                    txtDisplay.Text = (fv - sv).ToString();
+                    break;
+                case "+":
+                    txtDisplay.Text = (fv + sv).ToString();
+                    break;
+            }
+            Session.Clear();
+            Session["FirstValue"] = txtDisplay.Text;
+            Session["num"] = "y";
+            } catch { }
+        }
+
+        protected void btn9_Click(object sender, EventArgs e)
+        {
+            AssignValue(9);
+        }
+        protected void btn8_Click(object sender, EventArgs e)
+        {
+            AssignValue(8);
+        }
+        protected void btn7_Click(object sender, EventArgs e)
+        {
+            AssignValue(7);
+        }
+        protected void btn6_Click(object sender, EventArgs e)
+        {
+            AssignValue(6);
+        }
+        protected void btn5_Click(object sender, EventArgs e)
+        {
+            AssignValue(5);
+        }
+
+        protected void btn4_Click(object sender, EventArgs e)
+        {
+            AssignValue(4);
+        }
+        protected void btn3_Click(object sender, EventArgs e)
+        {
+            AssignValue(3);
+        }
+        protected void btn2_Click(object sender, EventArgs e)
+        {
+            AssignValue(2);
+        }
+        protected void btn1_Click(object sender, EventArgs e)
+        {
+            AssignValue(1);
+        }
+        protected void btn0_Click(object sender, EventArgs e)
+        {
+            AssignValue(0);
+        }
+
+        public float AssignValue(float buttonVal)
+        {
+            string numcheck = Session["num"].ToString();
+            try
+            {
+                string op = Session["op"].ToString();
+                try
+                {
+                    float sv = float.Parse(Session["SecondValue"].ToString());
+                    object dec = Session["decimal"];
+                    switch (dec)
+                    {
+                        case "on":
+                            Session["SecondValue"] = sv.ToString() + "." + buttonVal.ToString();
+                            Session["decimal"] = "off";
+                            break;
+                        default:
+                            Session["SecondValue"] = sv.ToString() + buttonVal.ToString();
+                            break;
+                    }
+                }
+                catch
+                {
+                    object dec = Session["decimal"];
+                    switch (dec)
+                    {
+                        case "on":
+                            Session["SecondValue"] = "." + buttonVal;
+                            Session["decimal"] = "off";
+                            break;
+                        default:
+                            Session["SecondValue"] = buttonVal;
+                            break;
+                    }
+                }
+            }
+            catch
+            {
+                
+                if (numcheck == "y" || txtDisplay.Text.Trim( ) == "0")
+                {
+                    object dec = Session["decimal"];
+                    switch (dec)
+                    {
+                        case "on":
+                            Session["FirstValue"] = "." + buttonVal;
+                            Session["decimal"] = "off";
+                            break;
+                        default:
+                            Session["FirstValue"] = buttonVal;
+                            break;
+                    }
+                    Session["num"] = "n";
+                }
+                else
+                {
+                    float fv = float.Parse(Session["FirstValue"].ToString());
+                    object dec = Session["decimal"];
+                    switch (dec)
+                    {
+                        case "on":
+                            Session["FirstValue"] = fv.ToString() + "." + buttonVal.ToString();
+                            Session["decimal"] = "off";
+                            break;
+                        default:
+                            Session["FirstValue"] = fv.ToString() + buttonVal.ToString();
+                            break;
+                    }
+                }
+            }
+
+            string p1, p2, p3;
+            try
+            {
+                p1 = Session["FirstValue"].ToString() + " ";
+            }
+            catch
+            {
+                p1 = "0";
+            }
+            try
+            {
+                p2 = Session["op"].ToString() + " ";
+            }
+            catch
+            {
+                p2 = "";
+            }
+            try
+            {
+                p3 = Session["SecondValue"].ToString();
+            }
+            catch
+            {
+                p3 = "";
+            }
+            txtDisplay.Text = p1 + p2 + p3;
+            return buttonVal;
+        }
+
+        protected void dec_Click(object sender, EventArgs e)
+        {
+            if (Session["decimal"] == null)
+            {
+                if (Session["num"].ToString() == "y")
+                {
+                    AssignValue(0);
+                    Session["num"] = "n";
+                }
+                Session["decimal"] = "on";
+            } else if (Session["decimal"].ToString() == "off"){ }
+            else
+            {
+                if (Session["num"].ToString() == "n")
+                {
+                    Session["num"] = "y";
+                }
+                Session["decimal"] = null;
+            }
+        }
+    }
+}
